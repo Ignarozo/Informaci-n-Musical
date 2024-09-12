6db7694de3accbd8df09c026fa8e32c5ee7caeb0
Ignarozo <rozonb1@gmail.com>
2024-09-12 13:06:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function mostrarSeguidos() {
-    document.getElementById("Seguidos").style.display = "block";
+    document.getElementById("Seguidos").style.display = "grid";
     document.getElementById("Principal").style.display = "none";
     document.getElementById("Tendencias").style.display = "none";
 }
 
 function mostrarTendencias() {
-    document.getElementById("Tendencias").style.display = "block";
+    document.getElementById("Tendencias").style.display = "grid";
     document.getElementById("Principal").style.display = "none";
     document.getElementById("Seguidos").style.display = "none";
 }
 
 function mostrarPrincipal() {
-    document.getElementById("Principal").style.display = "block";
+    document.getElementById("Principal").style.display = "grid";
     document.getElementById("Seguidos").style.display = "none";
     document.getElementById("Tendencias").style.display = "none";
 }
